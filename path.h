c75e6b1566e52c172609539e132a33de75219dbb
redjo99 <49435750+redjo99@users.noreply.github.com>
2019-12-02 14:12:40 +0900

    Update path.h

--- path.h
+++ path.h
@@ -24,7 +24,7 @@ typedef struct Date {			// 날짜별로 이름, 시간, 분을 포함
 
 void setDepartureTime(int* t);		// 출발시각 31개 랜덤생성
 void setDate(Date* date, int* t);	// 랜덤 생성한 시간을 시,분으로 나누어 노드에 저장
-void printDate(Date* date);
+void printDate(Graph* graph);
 
 Dest* findNode(Graph* graph, int s, int d);
 
